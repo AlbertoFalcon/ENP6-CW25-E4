bcaf12c93cb985b8177bf01172fffab99649aa5c
citlalli-chavez8998 <324322233@alumno.enp.unam.mx>
2025-06-26 21:06:18 -0600

    commit pa q funcione el pull

--- Dynamics/JS/funcionesDeBusqueda.js
+++ Dynamics/JS/funcionesDeBusqueda.js
@@ -198,8 +198,6 @@ function getCancionesPorIdAlbum(idAlbum) {
   return canciones;
 }
 //funcion para buscar canciones por genero con base en el id del genero
-<<<<<<< HEAD
-=======
 function getCancionesPorIdGenero(idGenero) {
   let canciones = [];
   for (let i = 0; i < baseDatosJSON.canciones.length; i++) {
@@ -209,4 +207,3 @@ function getCancionesPorIdGenero(idGenero) {
   }
   return canciones;
 }
->>>>>>> dfa097f88916e725bd649b0da852564e7486a61d
